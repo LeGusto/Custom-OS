3721a3522eba503a317841b41750fdc0339ca7e5
= <janusonis.gustas@gmail.com>
2025-10-22 22:50:46 +0300

    adde make file where main.asm is compiled to main.bin and then it is pushed to floppy_image.img, finished very simple bootloader that prints a message and halts

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,61 @@
-ORG 0x7C00 // offsets to OS memory location
-BITS 16 // register width
+ORG 0x7C00 ; offsets to OS memory location
+BITS 16 ; register width
+
+; CR + LF, moves cursor to beginning + moves cursor down one line
+%define endl 0x0D, 0x0A
+
+start:
+    jmp main
+
+puts:
+    ; save values to stack
+    PUSH si
+    PUSH ax 
+
+.loop:
+    LODSB ; sets al = [DS:SI], then increments SI. LODSB loads a singly byte to al, which is the lower half of ax, and each character is 1 byte
+    OR al, al ; checks if the current byte is a terminate char (null), because it sets the Z flag to 1 when the result is 0
+    JZ .done ; jumps to .done when Z flag is 1
+
+    MOV bh, 0x0 ; set page number
+    MOV ah, 0x0E ; write in TTY mode service from the video interrupt (essentially prints al at cursor, moves cursor)
+    INT 10h ; trigger video interrupt
+
+    JMP .loop
+
+
+.done:
+    ; return the values from the stack
+    POP ax
+    POP si
+    ret
 
 main:
-    HLT // immediatelly stop the CPU
 
-.halt // failsafe in case HLT doesn't work to prevent reading random memory
+    ; set data segments to beginning
+    MOV ax, 0 ; cannot write 0 directly to ds, es
+    MOV ds, ax
+    MOV es, ax
+
+    ; set stack to beginning (grows downwards + PUSH first decrements, so won't overwrite)
+    MOV ss, ax
+    MOV sp, 0x7C00
+
+    MOV si, msg
+    CALL puts
+
+    ; immediatelly stop the CPU
+    HLT
+
+; failsafe in case HLT doesn't work to prevent reading random memory
+.halt:
     JMP .halt
 
-TIMES 500-($-$$) DB 0
+
+msg db 'LESS GOOOOOOOO!', endl, 0
+
+; pads OS program with 0s
+TIMES 510-($-$$) DB 0
+
+; insert code
 DW 0AA55h
